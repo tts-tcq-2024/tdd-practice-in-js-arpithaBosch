52f6222087ab5c1316056221dcc6abada80fa5fc
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-09-25 15:31:11 +0530

    Update stringCalculator.test.js

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,9 +3,16 @@ const { add } = require('../src/stringCalculator');
 describe('StringCalculator Algorithm', () => {
 
     describe('stringCalculatorTests', () => {
-           it('should return 0 for an empty string', () => {
+        //The calculator should return 0 when an empty string is passed.
+        it('should return 0 for an empty string', () => {
            const result = add("");
            expect(result).to.equal(0);
-        });   
+        });  
+        
+       // The calculator should return the same number when a single number is passed. 
+        it('should return the number itself for a single number input', () => {
+            expect(add("1")).to.equal(1);
+            expect(add("25")).to.equal(25);
+          });
     });
 });
