1297bfd3de22e21f53c8b29a38bdea06479481ea
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-09-25 15:05:18 +0530

    Update stringCalculator.test.js

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,30 +3,9 @@ const { add } = require('../src/stringCalculator');
 describe('StringCalculator Algorithm', () => {
 
     describe('stringCalculatorTests', () => {
-      
-        it('expectZeroForEmptyInput', () => {
-            expect(add("")).to.equal(0);
-        });
-
-        it('expectZeroForSingleZero', () => {
-              expect(add("0")).to.equal(0);
-        });
-
-        it('expectSumForTwoNumbers', () => {
-             expect(add("1,2")).to.equal(3);
-        });
-      
-        it('ignoreNumbersGreaterThan1000', () => {
-             expect(add("1,1001")).to.equal(1);
-        });
-      
-        it('expectSumWithCustomDelimiter', () => {
-             expect(add("//;\n1;2")).to.equal(3);
-        });
-      
-        it('expectSumWithNewlineDelimiter', () => {
-             expect(add("1\n2,3")).to.equal(6);
-        });
-      
+           it('should return 0 for an empty string', () => {
+           const result = add("");
+           expect(result).to.equal(0);
+        });   
     });
 });
