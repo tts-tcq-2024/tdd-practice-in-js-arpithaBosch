f7d82505656a696e3c8a787e642ba13763016dd4
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-09-25 15:36:59 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,9 +1,22 @@
-// StringCalculator function to handle empty input
 function add(numbers) {
-  if (numbers === "") {
-    return 0;
+  if (numbers === "") return 0;
+
+  const numArray = splitNumbers(numbers);
+  const singleNumberResult = handleSingleNumber(numArray);
+  if (singleNumberResult !== null) return singleNumberResult;
+
+  return 0;
+}
+
+function splitNumbers(numbers) {
+  return numbers.split(",");
+}
+
+function handleSingleNumber(numArray) {
+  if (numArray.length === 1) {
+    return parseInt(numArray[0]);
   }
+  return null;
 }
 
 module.exports = { add };
-
